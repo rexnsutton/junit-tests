efe3d6c2871797b9ac85cf21c85d27be4d4a7832
Student Name <rexsutton2@gmail.cogit>
2020-05-21 09:06:49 -0500

    finished

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Student {
+    private long id;
+    private String name;
+    private ArrayList <Integer> grades = new ArrayList<>();
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+//        this.grades = grades;
+    }
+
+    public long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public ArrayList<Integer> getGrades(){
+        return this.grades;
+    }
+    public void addGrade (int grade){
+        this.grades.add(grade);
+    }
+//    public double getAverage(){
+//
+//    }
+
+
+
+//    public static void main(String[] args) {
+//        Student rex = new Student(66666666, "rex");
+//    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class StudentTest {
+  Student rex;
+  @Before
+  public void setUp(){
+    rex = new Student(6666666,"rex");
+
+  }
+
+  @Test
+    public void testGetId(){
+      assertEquals(6666666, rex.getId());
+  }
+  @Test
+  public void testGetName(){
+    assertEquals("rex", rex.getName());
+  }
+  @Test
+  public void testIfGradesWorks(){
+    assertEquals(0, rex.getGrades().size());
+  }
+  @Test
+  public void testIfAddGradeWorks(){
+    rex.addGrade(80);
+    assertTrue(80 == rex.getGrades().get(0));
+  }
+
+
+
+  @Test
+  public void testGetAverage(){
+    rex.addGrade(50);
+    rex.addGrade(60);
+    assertEquals(55, (rex.getGrades().get(0) + rex.getGrades().get(1)) / rex.getGrades().size());
+  }
+
 }
+
+
+
